d5398c4bc0d27e2ae0869f919bdebb7214eee657
mzhao8 <46694702+mzhao8@users.noreply.github.com>
2021-11-20 16:10:34 +0000

    updated cron job

--- main.py
+++ main.py
@@ -11,26 +11,31 @@ from time import sleep
 url = "https://www.wonderland.money/"
 api_key = os.environ['key']
 
-chrome_options = Options()
-chrome_options.add_argument('--no-sandbox')
-chrome_options.add_argument('--disable-dev-shm-usage')
-driver = webdriver.Chrome(options=chrome_options)
+while True:
+    try:
+        chrome_options = Options()
+        chrome_options.add_argument('--no-sandbox')
+        chrome_options.add_argument('--disable-dev-shm-usage')
+        driver = webdriver.Chrome(options=chrome_options)
 
-driver.get(url)
-driver.maximize_window()
+        driver.get(url)
+        driver.maximize_window()
 
-time.sleep(5)
+        time.sleep(5)
 
-current_time = datetime.datetime.now()
+        current_time = datetime.datetime.now()
 
-page_source = driver.page_source
+        page_source = driver.page_source
 
-with open("test.html", "w") as f:
-    f.write(driver.page_source)
-    
-print(f"refreshed on {current_time}")
+        with open("test.html", "w") as f:
+            f.write(driver.page_source)
+            
+        print(f"refreshed on {current_time}")
 
-get_price(api_key)
+        get_price(api_key)
 
-update_csv("test.csv")
+        update_csv("test.csv")
 
+        time.sleep(7200)
+    except:
+        print(f'error at {current_time}')
